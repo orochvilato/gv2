17c64941f084f41af558ef618bb6da405c56a990
Olivier ROCH VILATO <olivierrochvilato@yahoo.com>
2018-03-10 21:21:31 -0400

    multichromes

--- genvisu/controllers/generateur.py
+++ genvisu/controllers/generateur.py
@@ -21,24 +21,36 @@ def savepage(url,size,key):
     from selenium import webdriver
 
 
+    chrome_count = memcache.get('chrome_count')
+    if not chrome_count:
+        memcache.set('chrome_count',0,120)
+        chrome_count = 0
+    wait = 5
 
+    while chrome_count>1:
+        wait += 1
+        memcache.set(key,{'etat':u'En attente','avancement':wait})
+        time.sleep(0.5)
+        chrome_count = memcache.get('chrome_count') or 0
+
+    memcache.incr('chrome_count',1)
     options = webdriver.ChromeOptions()
     options.binary_location = '/usr/bin/google-chrome'
     options.add_argument('headless')
     options.add_argument('window-size=%sx%s' % (size[0],size[1]+100))
     cdservice = service.Service('/usr/bin/chromedriver')
-    memcache.set(key,{'etat':u'génération du visuel','avancement':20})
+    memcache.set(key,{'etat':u'Génération du visuel','avancement':20})
     cdservice.start()
     driver = webdriver.Chrome(chrome_options=options)
-    memcache.set(key,{'etat':u'génération du visuel','avancement':30})
+    memcache.set(key,{'etat':u'Génération du visuel','avancement':30})
 
     driver.get(url);
-    memcache.set(key,{'etat':u'génération du visuel','avancement':40})
+    memcache.set(key,{'etat':u'Génération du visuel','avancement':40})
     #time.sleep(5) # Let the user actually see something!
 
     for i in range(3):
         time.sleep(1)
-        memcache.set(key,{'etat':u'génération du visuel','avancement':60+i*10})
+        memcache.set(key,{'etat':u'Génération du visuel','avancement':60+i*10})
 
     im = Image.open(StringIO.StringIO(driver.get_screenshot_as_png()))
     im2 = im.crop((0,0,size[0],size[1]))
@@ -46,5 +58,7 @@ def savepage(url,size,key):
     cdservice.stop()
     output = StringIO.StringIO()
     im2.save(output,'PNG')
-    memcache.set(key,{'etat':u'génération du visuel','avancement':100})
+    memcache.set(key,{'etat':u'Génération du visuel','avancement':100})
+
+    memcache.decr('chrome_count',1)
     return output.getvalue()
