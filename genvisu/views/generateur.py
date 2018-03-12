0723a5e340a7b34770c075b5586089e0c3e40ee5
Olivier ROCH VILATO <olivier.rochvilato@chilbp.fr>
2018-03-12 14:30:24 -0400

    recup dimension via balise meta

--- genvisu/views/generateur.py
+++ genvisu/views/generateur.py
@@ -26,7 +26,13 @@ def senddata():
 @app.route('/edit/<visuelid>')
 def generateur(visuelid):
     if request.method == 'GET':
-        # !! récuperer les dimensions du visuel (balise META)
+        path_visuel = os.path.join(app_path,'genvisu','modeles',visuels[visuelid],'index.html')
+        doc = open(path_visuel,'r').read()
+        import re
+        m = re.search(r'<meta dimension="([0-9]+x[0-9]+)">',doc,re.MULTILINE)
+        if m:
+            width,height = [int(x) for x in m.groups()[0].split('x')]
+        
         return render_template('generateur.html', visuel_path='/visuel/'+visuelid , width=100, height=100)
 
 def returnfile(folder,_file):
